e5c4aaddd6c0d613997a05a0a3c3411ab040d8be
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2016-06-30 10:26:53 -0700

    (fix) Fixed configurations and added circle.yml
    (bug) Fixed reference error
    
    (fix) updated gitignore

---
+++ server/apiConfig.js
@@ -0,0 +1,10 @@
+export const apiKey = {
+  "uid": "ENTER_ME",
+  "token": "ENTER_ME",
+  "apiToken": "ENTER_ME"
+}
+
+export const apiRoutes = {
+  getAll: 'https://prod-api.level-labs.com/api/v2/core/get-all-transactions',
+  getProjections: 'https://prod-api.level-labs.com/api/v2/core/projected-transactions-for-month'
+}
